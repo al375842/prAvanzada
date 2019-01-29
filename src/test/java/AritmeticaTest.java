91b8222c980209a838b602d7fd8a16d8f889598e
Lucas Niño Ruiz <al375842@aula1154act.sg.uji.es>
2019-01-29 14:19:31 +0100

    Usada la anotación @Before.

--- src/test/java/AritmeticaTest.java
+++ src/test/java/AritmeticaTest.java
@@ -1,31 +1,40 @@
 import aritmetica.Aritmetica;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AritmeticaTest {
+    private Aritmetica aritmetica;
+
+    @Before  //antes de cualquier test
+    public void inicio() {
+        System.out.println("Método inicial");
+        aritmetica = new Aritmetica();
+    }
+
     @Test
     public void sumaTest() { //lista de args debe estar vacía
         //fail("Esta prueba debe fallar");
-        Aritmetica aritmetica = new Aritmetica();
+        System.out.println("Método sumaTest");
         assertEquals(2, aritmetica.suma(1,1),0);
     }
 
     @Test
     public void restaTest() {
-        Aritmetica aritmetica = new Aritmetica();
+        System.out.println("Método restaTest");
         assertEquals(0, aritmetica.resta(1,1),1);
     }
 
     @Test
     public void multiplicaciónTest() {
-        Aritmetica aritmetica = new Aritmetica();
+        System.out.println("Método multiplicaciónTest");
         assertEquals(6,aritmetica.multiplicacion(2,3),0);
     }
 
     @Test
     public void divisiónTest() {
-        Aritmetica aritmetica = new Aritmetica();
+        System.out.println("Método divisiónTest");
         assertEquals(5, aritmetica.division(10,2),0);
     }
 }
