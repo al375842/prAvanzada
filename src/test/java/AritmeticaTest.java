898894deafeb326e5af0e0247deecc8ae5ba4bf0
Lucas Niño Ruiz <al375842@aula1154act.sg.uji.es>
2019-01-29 14:03:19 +0100

    Método de prueba para suma y resta.

---
+++ src/test/java/AritmeticaTest.java
@@ -0,0 +1,28 @@
+import aritmetica.Aritmetica;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AritmeticaTest {
+    @Test
+    public void sumaTest() { //lista de args debe estar vacía
+        //fail("Esta prueba debe fallar");
+        Aritmetica aritmetica = new Aritmetica();
+        assertEquals(2, aritmetica.suma(1,1),0);
+    }
+
+    @Test
+    public void restaTest() {
+        Aritmetica aritmetica = new Aritmetica();
+        assertEquals(0, aritmetica.resta(1,1),1);
+    }
+
+    @Test
+    public void multiplicaciónTest() {
+
+    }
+    @Test
+    public void divisiónTest() {
+
+    }
+}
